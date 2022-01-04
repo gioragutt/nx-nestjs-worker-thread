331e183b9634c2c002444b300cefbcdbb7b6e313
Giora Guttsait <giora111@gmail.com>
2022-01-05 00:48:01 +0200

    replace app.resolve with app.get

--- apps/api/src/app/worker/main.ts
+++ apps/api/src/app/worker/main.ts
@@ -4,9 +4,8 @@ import { WorkerModule } from './worker.module';
 import { WorkerService } from './worker.service';
 
 async function bootstrap() {
-  const app = await NestFactory.create(WorkerModule);
-  const service = await app.resolve(WorkerService);
-  const response = service.getData();
+  const app = await NestFactory.createApplicationContext(WorkerModule);
+  const response = app.get(WorkerService).getData();
   parentPort.postMessage(response);
 }
 
